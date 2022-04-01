582921caf9d1f5e4427dbbeaf4b6d6f186201017
harsh <merikahani1207@gmail.com>
2022-04-01 23:53:51 +0530

    MY Java Class

---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("This is My First Java Project");
+
+        System.out.println("Which is Start from 28.03.2022");
+
+    }
+}
---
+++ src/Variables/DemoVariables.java
@@ -0,0 +1,28 @@
+package Variables;
+
+public class DemoVariables {
+    public static void main(String[] args) {
+        System.out.println("Let's Understand Variables in JAVA");
+        //syntex : DataType var_name = Value;
+
+
+        String City = "AGRA";
+
+        String Name = "Harsh Vardhan";
+
+        String Designation = "Computer Operator";
+
+        double Experience = 6.5;
+
+        int Salary = 25000;
+
+        System.out.println("My Name Is = " +Name);
+        System.out.println("Current city where I'm working in = " +City);
+        System.out.println("My Current Designation is = " +Designation);
+        System.out.println("Total Experience = " +Experience);
+        System.out.println("I got Salary = " +Salary);
+
+
+
+    }
+}
