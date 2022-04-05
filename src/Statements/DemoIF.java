6eb9f23656d0fbf98fd70f10550150295c4742e8
harsh <merikahani1207@gmail.com>
2022-04-05 21:49:01 +0530

    update on 05.04.2022

---
+++ src/Statements/DemoIF.java
@@ -0,0 +1,26 @@
+package Statements;
+
+public class DemoIF {
+    public static void main(String[] args) {
+        int A= 25;
+        String City = "Delhi";
+
+        if(A>=21){
+            System.out.println("I'm Eligible for Driving License");
+        }
+        else{
+            System.out.println("I'm not Eligible for Driving License");
+            }
+
+        if(City=="Agra") {
+            System.out.print("Currently I'm in Agra City");
+        }
+            else{
+                System.out.println("Currently I'm not in Agra City");
+            }
+
+        }
+}
+
+
+
