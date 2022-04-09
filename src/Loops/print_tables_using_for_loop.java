a0bc906328073e9dd97e8dd0b9735c96a7c61270
harsh <merikahani1207@gmail.com>
2022-04-09 20:08:29 +0530

    update on 09.04.2022

--- src/Loops/print_tables_using_for_loop.java
+++ src/Loops/print_tables_using_for_loop.java
@@ -1,14 +1,16 @@
 package Loops;
 
 public class print_tables_using_for_loop {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int i;
-        int a=2;
+        int a = 2;
         System.out.println("Let's Print Table of Two");
 
-        for(i=1;i<=10;i++){
-
-            System.out.println( +a+" X "+i+" = " +a*i);
+        for (i = 1; i <= 10; i++) {
+            System.out.println(+a + " X " + i + " = " + a * i);
+            if (i == 5) {
+                break;
             }
+        }
     }
 }
